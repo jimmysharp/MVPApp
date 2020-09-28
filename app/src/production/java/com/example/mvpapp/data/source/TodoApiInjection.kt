2dd348e665d3fda52c3c8444a967eefe6cb9594e
sharp <jimmy.sharptech@gmail.com>
2020-09-28 14:35:41 +0900

    [Add] BASIC認証を追加

--- app/src/production/java/com/example/mvpapp/data/source/TodoApiInjection.kt
+++ app/src/production/java/com/example/mvpapp/data/source/TodoApiInjection.kt
@@ -1,7 +1,7 @@
 package com.example.mvpapp.data.source
 
 import com.squareup.moshi.Moshi
-import okhttp3.OkHttpClient
+import okhttp3.*
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -11,7 +11,12 @@ object TodoApiInjection {
 
     // アクセス先URL
     // エミュレータ実行PCのlocalhost:3000にサーバが立っているものとする
-    const val API_BASEURL = "https://10.0.2.2:3000/"
+    private const val API_BASEURL = "https://10.0.2.2:3000/"
+
+    // BASIC認証
+    // 今回はサンプルなのでハードコーディング
+    private const val AUTH_USER = "hoge"
+    private const val AUTH_PASSWORD = "Hoge"
 
     private val retrofit: Retrofit
     private val service: TodoApiService
@@ -21,6 +26,12 @@ object TodoApiInjection {
         loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
 
         val httpClient = OkHttpClient.Builder()
+            .authenticator { _, response ->
+                val credential = Credentials.basic(AUTH_USER, AUTH_PASSWORD)
+                response.request.newBuilder()
+                    .header("Authorization", credential)
+                    .build()
+            }
             .addInterceptor(loggingInterceptor)
             .readTimeout(30, TimeUnit.SECONDS)
             .build()
